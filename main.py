98d1a925392d1eb3566a320c0e83f14116045c3c
TassiloBalbo <contact@fsikora.com>
2021-12-19 01:58:19 +0100

    :fire: idk what im doin

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import socket
 import pynetstring
 import traceback
 import sys
+import base64
 
 HOST = '159.89.4.84'
 PORT = 42069
-NICK = 'tassilobalbo'
+NICK = b'tassilobalbo'
 AUTH_TOKEN = ''
 DATA_PORT = int()
+
+meme = 'meme.png'
 
 def main():
     try:
@@ -18,6 +21,10 @@ def main():
     except Exception:
         traceback.print_exc(file=sys.stdout)
     sys.exit(0)
+
+
+def recvStr(s: socket) -> str:
+    return pynetstring.decode(s.recv(1024))[0].decode('utf-8').split(' ')[1]
 
 
 def init() -> socket:
@@ -31,14 +38,44 @@ def init() -> socket:
 
 
 def auth(s: socket):
-    s.sendall(pynetstring.encode(b'C NICK'))
+    s.sendall(pynetstring.encode(b'C ' + NICK))
     res = pynetstring.decode(s.recv(1024))[0].decode('utf-8')
     res2 = pynetstring.decode(s.recv(1024))[0].decode('utf-8')
     AUTH_TOKEN = res.split(' ')[1]
     DATA_PORT = int(res2.split(' ')[1])
     print(AUTH_TOKEN, DATA_PORT)
     print('auth complete')
+    # Open Data Channel
+    dataChannel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dataChannel.connect((HOST, DATA_PORT))
+    dataChannel.sendall(pynetstring.encode(b'C ' + NICK))
+    print('Data Channel auth response: ' + recvStr(dataChannel))
+    handleReq(dataChannel)
 
+
+def handleReq(s):
+    for i in range(4):
+        res = recvStr(s)
+        print(res)
+        if res == 'REQ:meme':
+            sendMeme(s, meme)
+            print('meme sent')
+        elif res == 'REQ:description':
+            s.sendall(pynetstring.encode(b'C test'))
+            print('description sent')
+        elif res == 'REQ:isNSFW':
+            s.sendall(pynetstring.encode(b'C false'))
+            print('nsfw sent')
+        elif res == 'REQ:password':
+            s.sendall(pynetstring.encode(b'C kokos'))
+            print('pass sent')
+        print('datalength: ', pynetstring.decode(s.recv(1024)))
+    print('dtoken: ', pynetstring.decode(s.recv(1024)))
+
+def sendMeme(s, meme):
+    with open(meme, 'rb') as f:
+        data = base64.b64encode(f.read())
+    s.sendall(pynetstring.encode(b'C ' + data))
 
 if __name__ == '__main__':
     main()
